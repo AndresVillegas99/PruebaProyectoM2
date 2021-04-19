8d9d12332d1a094d5e71deb671ce1caa0eb9270e
Andres Villegas <villegasa25@gmail.com>
2021-04-18 20:23:46 -0600

    Cambios a funcion http

--- FuncionColas/Function1.cs
+++ FuncionColas/Function1.cs
@@ -8,7 +8,7 @@ namespace FuncionColas
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([QueueTrigger("myqueue-items", Connection = "DefaultEndpointsProtocol=https;AccountName=serviciocinecola;AccountKey=hOX66/XoxGr9EhBeJwc6fcNZSU/m9pgziyl8nDDgAfuRKozdipQDjKMJh5JpKNAYhwwbaUMIGD2GAXS3gqC0KA==;EndpointSuffix=core.windows.net")]string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("myqueue-items", Connection = "")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
