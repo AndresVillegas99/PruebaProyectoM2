76ea55f692b43104ee3547ced8e1c4271879060e
Andres Villegas <villegasa25@gmail.com>
2021-04-24 19:24:30 -0600

    Cambios a modelo captura y HTML de layout

--- Proyecto Cine metodologia/Models/Captura.cs
+++ Proyecto Cine metodologia/Models/Captura.cs
@@ -7,5 +7,13 @@ namespace Proyecto_Cine_metodologia.Models
         public string Email { get; set; }
         public string Nombre { get; set; }
 
+        public string Pelicula { get; set; }
+
+        public string Sala { get; set; }
+
+        public string Asiento { get; set; }
+
+        public string Precio { get; set; }
+
     }
 }
